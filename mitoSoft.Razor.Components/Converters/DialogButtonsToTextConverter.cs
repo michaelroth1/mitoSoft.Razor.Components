5f791e8a17c887589596fb383769961ee87c6d51
Michael <michael.mr.roth@live.com>
2021-12-07 14:33:12 +0100

    revert

---
+++ mitoSoft.Razor.Components/Converters/DialogButtonsToTextConverter.cs
@@ -0,0 +1,31 @@
+﻿using mitoSoft.Razor.Components.Enums;
+
+namespace mitoSoft.Razor.Components.Converters
+{
+    internal static class DialogButtonsToTextConverter
+    {
+        public static string ToNoText(this DialogButtons dialogResult)
+        {
+            if (dialogResult == DialogButtons.OkCancle)
+            {
+                return Properties.Resources.Cancle;
+            }
+            else
+            {
+                return Properties.Resources.No;
+            }            
+        }
+
+        public static string ToYesText(this DialogButtons dialogResult)
+        {
+            if (dialogResult == DialogButtons.YesNo)
+            {
+                return Properties.Resources.Yes;
+            }
+            else
+            {
+                return "OK";
+            }
+        }
+    }
+}
---
+++ mitoSoft.Razor.Components/Enums/DialogResult.cs
@@ -0,0 +1,11 @@
+﻿namespace mitoSoft.Razor.Components.Enums
+{
+    public enum DialogResult
+    {
+        None,
+        OK,
+        Cancle,
+        Yes,
+        No,
+    }
+}
